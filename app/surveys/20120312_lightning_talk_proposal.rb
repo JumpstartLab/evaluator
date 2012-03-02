508c67aa66e7a2db00f6c5e36c71b2aef63641f0
Jeff Casimir <jeff@casimircreative.com>
2012-03-02 15:21:43 -0500

    OSS and Lightning Talk proposals

---
+++ app/surveys/20120312_lightning_talk_proposal.rb
@@ -0,0 +1,10 @@
+survey "Lightning Talk Plan for Week of 3/12/12" do
+  question "What are you topic are you going to present on?"
+  answer :text
+
+  question "What do you expect people to know about the topic before your presentation?"
+  answer :text
+
+  question "What will they know after? How can we measure your success?"
+  answer :text
+end
