a23b2fb0009e52db0ba462f54d5c995d891d3129
Jeff Casimir <jeff@casimircreative.com>
2012-03-02 15:16:32 -0500

    Draft open-source plan

---
+++ app/surveys/notes/20120312_open_source_plan.rb
@@ -0,0 +1,13 @@
+survey "Open Source Plan for Week of 3/12/12" do
+  question "What's the Github URL for the project you want to work on?"
+  answer :string
+
+  question "What do you expect to accomplish in five hours of work?"
+  answer :string
+
+  question "How do you know it's worth working on? (issues, needs, etc)"
+  answer :text
+
+  question "How should your success be evaluated?"
+  answer :string
+end
