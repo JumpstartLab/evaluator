cf1a2284ab10e84d399ba2dbd75b73fb78752931
Jeff Casimir <jeff@casimircreative.com>
2012-03-12 08:54:10 -0400

    You need sections 'round your questions

--- app/evaluations/20120312_open_source_plan.rb
+++ app/evaluations/20120312_open_source_plan.rb
@@ -1,17 +1,19 @@
 evaluation "Open Source Plan for Week of 3/12/12" do
-  question "What's the Github URL for the project you want to work on?" do
-    free_response :string
-  end
+  section "Plan" do
+    question "What's the Github URL for the project you want to work on?" do
+      free_response :string
+    end
 
-  question "What do you expect to accomplish in five hours of work?" do
-    free_response :text
-  end
+    question "What do you expect to accomplish in five hours of work?" do
+      free_response :text
+    end
 
-  question "How do you know it's worth working on? (issues, needs, etc)" do
-    free_response :text
-  end
+    question "How do you know it's worth working on? (issues, needs, etc)" do
+      free_response :text
+    end
 
-  question "How should your success be evaluated?" do
-    free_response :text
+    question "How should your success be evaluated?" do
+      free_response :text
+    end
   end
 end
