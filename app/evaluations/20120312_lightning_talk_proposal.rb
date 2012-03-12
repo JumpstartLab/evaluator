4856e65911683104a38653be46278d7d7889daee
Jeff Casimir <jeff@casimircreative.com>
2012-03-12 08:22:33 -0400

    Revising Lightning Talk and OSS Plan for new API

--- app/evaluations/20120312_lightning_talk_proposal.rb
+++ app/evaluations/20120312_lightning_talk_proposal.rb
@@ -1,10 +1,13 @@
-assessment "Lightning Talk Plan for Week of 3/12/12" do
-  question "What are you topic are you going to present on?"
-  answer :text
+evaluation "Lightning Talk Plan for Week of 3/12/12" do
+  question "What are you topic are you going to present on?" do
+    free_response :text
+  end
 
-  question "What do you expect people to know about the topic before your presentation?"
-  answer :text
+  question "What do you expect people to know about the topic before your presentation?" do
+    free_response :text
+  end
 
-  question "What will they know after? How can we measure your success?"
-  answer :text
+  question "What will they know after? How can we measure your success?" do
+    free_response :text
+  end
 end
--- app/evaluations/20120312_open_source_plan.rb
+++ app/evaluations/20120312_open_source_plan.rb
@@ -1,13 +1,17 @@
-assessment "Open Source Plan for Week of 3/12/12" do
-  question "What's the Github URL for the project you want to work on?"
-  answer :string
+evaluation "Open Source Plan for Week of 3/12/12" do
+  question "What's the Github URL for the project you want to work on?" do
+    free_response :string
+  end
 
-  question "What do you expect to accomplish in five hours of work?"
-  answer :string
+  question "What do you expect to accomplish in five hours of work?" do
+    free_response :text
+  end
 
-  question "How do you know it's worth working on? (issues, needs, etc)"
-  answer :text
+  question "How do you know it's worth working on? (issues, needs, etc)" do
+    free_response :text
+  end
 
-  question "How should your success be evaluated?"
-  answer :string
+  question "How should your success be evaluated?" do
+    free_response :text
+  end
 end
