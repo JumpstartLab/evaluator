200b4c1e175ee7317e6eed6f646c64d32a79a6ac
Matt Yoho <mby@mattyoho.com>
2012-03-26 14:05:10 -0400

    Whitespace

--- app/evaluations/20120326_reading_assessment.rb
+++ app/evaluations/20120326_reading_assessment.rb
@@ -2,13 +2,12 @@ evaluation "Reading Assessment for HTML & CSS" do
   section "Peer Review" do
     question "Which chapter(s) are you reviewing?" do
       free_response :string
-    end 
+    end
 
     Person.students.each do |person|
-      question "Score for #{person} (0-3)", 
-               {:github_handle => person.github_handle, 
-                :person_id => person.id} do
-      	free_response :integer
+      question "Score for #{person} (0-3)", github_handle: person.github_handle,
+                                            person_id:     person.id do
+        free_response :integer
       end
     end
   end
