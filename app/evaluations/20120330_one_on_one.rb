c56ed4765353855109e77c372a470220a1eab9c1
Jeff Casimir <jeff@casimircreative.com>
2012-03-30 12:50:53 -0400

    One on One for 3-30

---
+++ app/evaluations/20120330_one_on_one.rb
@@ -0,0 +1,29 @@
+instructor_evaluation "1-on-1 for 3/30/12" do
+  section "Self-Evaluation" do  
+    question "Anything interesting from their self evaluation?" do
+      free_response :text
+    end
+
+    question "Discussion notes about self-eval:" do
+      free_response :text
+    end
+  end
+
+  section "Etc" do
+    question "How are things?" do
+      free_response :text
+    end
+
+    question "Any acute concerns?" do
+      free_response :text
+    end
+
+    question "Anything you want to reinforce or recognize?" do
+      free_response :text
+    end
+
+    question "Anything else?" do
+      free_response :text
+    end
+  end
+end
