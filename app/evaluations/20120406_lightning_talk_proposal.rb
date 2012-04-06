9c63ef07508dbd94f116f32d0804d7044c32f72f
Matt Yoho <mby@mattyoho.com>
2012-04-06 09:18:37 -0400

    Assessments are for Jeff's group

--- app/evaluations/20120406_lightning_talk_proposal.rb
+++ app/evaluations/20120406_lightning_talk_proposal.rb
@@ -1,4 +1,4 @@
-lightning_talk_proposal "mattyoho", "Lightning Talk Plan for 4/13" do
+lightning_talk_proposal "jcasimir", "Lightning Talk Plan for 4/13" do
   section "General" do
     free_response "What topic are you going to present on?", :kind => :text
     free_response "What do you expect people to know about the topic before your presentation?", :kind => :text
