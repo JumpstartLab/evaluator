b582b561c464d51115a890ee98df43aa58279b97
Jeff Casimir <jeff@casimircreative.com>
2012-04-06 12:22:58 -0400

    One-on-One eval with issue

---
+++ app/evaluations/20120406_one-on-one.rb
@@ -0,0 +1,11 @@
+instructor_evaluation "1-on-1 for 4/6/12" do
+  section "Self-Evaluation" do  
+    free_response "Topics to discuss from the self evaluation:", :kind => :text
+    free_response "Discussion notes about self-eval:", :kind => :text
+  end
+
+  section "Etc" do
+    free_response "How did the shadowing go? Project progress?", :kind => :text
+    free_response "Any acute concerns?", :kind => :text
+  end
+end
