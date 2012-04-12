3a9d222798ece5c21337da0046e848f503b9d09f
Jeff Casimir <jeff@casimircreative.com>
2012-04-12 14:51:38 -0400

    One-on-one script

---
+++ app/evaluations/20120412_one-on-one.rb
@@ -0,0 +1,11 @@
+instructor_evaluation "1-on-1 for 4/12/12" do
+  section "Self-Evaluation" do  
+    free_response "Topics to discuss from the self evaluation:", :kind => :text
+    free_response "Discussion notes about self-eval:", :kind => :text
+  end
+
+  section "Etc" do
+    free_response "Project progress?", :kind => :text
+    free_response "Any acute concerns?", :kind => :text
+  end
+end
