765022fae15785fe14f0ebdf1074edab123cab4e
Matt Yoho <mby@mattyoho.com>
2012-04-18 16:38:42 -0400

    Add open source proposal for 4/18

---
+++ app/evaluations/20120418_open_source_plan.rb
@@ -0,0 +1,19 @@
+evaluation "Open Source Plan for Week of 4/18/12" do
+  section "Last Week" do
+    pick_one "How well did your open source work go last week?" do
+      option "Amazing"
+      option "Good"
+      option "Not So Good"
+      option "Disaster"
+    end
+
+    free_response "Did you achieve your goal?", :kind => :text
+  end
+
+  section "Plan" do
+    free_response "What's the Github URL for the project you want to work on?", :kind => :text
+    free_response "What do you expect to accomplish in your work time?", :kind => :text
+    free_response "How do you know it's worth working on? (issues, needs, etc)", :kind => :text
+    free_response "How will you measure your success?", :kind => :text
+  end
+end
