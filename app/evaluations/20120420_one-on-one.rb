3e760bc200e013a0047de12bd653539bb79c1399
Matt Yoho <mby@mattyoho.com>
2012-04-20 13:48:57 -0400

    Add one-on-one for 4/20

---
+++ app/evaluations/20120420_one-on-one.rb
@@ -0,0 +1,9 @@
+instructor_evaluation "1-on-1 for 4/20/12" do
+  section "Self-Evaluation" do
+    free_response "Discussion notes about self-eval:", :kind => :text
+  end
+
+  section "Etc" do
+    free_response "Any acute concerns?", :kind => :text
+  end
+end
