6456250b1b92b13157b09de3b23093535e020091
Jeff Casimir <jeff@casimircreative.com>
2012-05-03 11:49:54 -0400

    Minor tweaks

--- app/evaluations/20120503_team_assessment.rb
+++ app/evaluations/20120503_team_assessment.rb
@@ -2,14 +2,14 @@ evaluation "Team Assessment for SonOfStoreEngine" do
   section 'Project Retrospective' do
     free_response "What went well during the project?", :kind => :text
     free_response "What went poorly during the project?", :kind => :text
-    free_response "Describe a learning experience from that you've taken away from the project. It can be related to programming, working with a team, process management, or other.", :kind => :text
+    free_response "Describe a learning experience that you've taken away from the project. It can be related to programming, working with a team, process management, or other.", :kind => :text
   end
 
   section 'Teammate Feedback' do
     description <<-DES
 For each of your teammates, describe what it was like working with them on SonOfStoreEngine.
 
-Talk about their communication style, the contributions, and where they fell down. Talk about ways in which you encouraged or discouraged working well with them, or how you interacted.
+Talk about their communication style, the contributions, and where they fell down. Talk about ways in which you succeeded or struggled in working with them, and how you interacted.
     DES
 
     section "Teammate 1" do
