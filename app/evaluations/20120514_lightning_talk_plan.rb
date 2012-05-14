05a8be063a8b84215e1a0a6232f4be1189d8dc4e
Jeff Casimir <jeff@jumpstartlab.com>
2012-05-14 14:02:45 -0400

    Lightning Talk Plan for this week

---
+++ app/evaluations/20120514_lightning_talk_plan.rb
@@ -0,0 +1,7 @@
+lightning_talk_proposal "group 2", "Lightning Talk Plan for 5/18" do
+  section "How do you design things people want to use?" do
+    free_response "What topic are you going to present on?", :kind => :text
+    free_response "What do you expect people to know about the topic before your presentation?", :kind => :text
+    free_response "What is the thesis of your presentation? IE, what is the central understanding or position of your talk?", :kind => :text
+  end
+end
