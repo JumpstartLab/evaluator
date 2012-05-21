9c61f8927a0ecad21fc27d505d0ca287496ef3ca
Jeff Casimir <jeff@casimircreative.com>
2012-05-21 08:11:16 -0400

    Skills Assessment revision

--- app/evaluations/20120518_skills_assessment.rb
+++ app/evaluations/20120518_skills_assessment.rb
@@ -1,11 +1,12 @@
-instructor_evaluation "Skills Assessment 5/18" do
+instructor_evaluation "Skills Assessment 5/21" do
   section "Skills" do
-    free_response "1. Class Decomposition", :kind => :integer, :group => :overall_score, :range => (1..3)
-    free_response "2. Method Naming & Design", :kind => :integer, :group => :overall_score, :range => (1..3)
-    free_response "3. Ruby Syntax", :kind => :integer, :group => :overall_score, :range => (1..3)
-    free_response "4. Debugging", :kind => :integer, :group => :overall_score, :range => (1..3)
-    free_response "5. Working with Documentation", :kind => :integer, :group => :overall_score, :range => (1..3)
-    free_response "6. System & Tools (CLI, git, etc)", :kind => :integer, :group => :overall_score, :range => (1..3)
+    free_response "1. Class Decomposition", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "2. Method Naming & Design", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "3. Ruby Syntax", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "4. Debugging", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "5. Working with Documentation", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "6. System & Tools (CLI, git, etc)", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "7. Responding to Feedback", :kind => :integer, :group => :overall_score, :range => (0..3)
     free_response "General comments:", :kind => :text
   end
 end
