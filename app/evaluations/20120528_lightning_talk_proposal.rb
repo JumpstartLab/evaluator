9344be1ba68c694d668044d7841b46314c5cb521
Jeff Casimir <jeff@casimircreative.com>
2012-05-28 22:04:34 -0400

    Lightning Talk for this week

---
+++ app/evaluations/20120528_lightning_talk_proposal.rb
@@ -0,0 +1,7 @@
+lightning_talk_proposal "group 3", "Lightning Talk Plan for 6/1" do
+  section "Linux tools for service Ruby/Rails Applications" do
+    free_response "What topic are you going to present on?", :kind => :text
+    free_response "What do you expect people to know about the topic before your presentation?", :kind => :text
+    free_response "What is the thesis of your presentation? IE, what is the central understanding or position of your talk?", :kind => :text
+  end
+end
