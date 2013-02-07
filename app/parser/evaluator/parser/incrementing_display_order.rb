7ae936e6e5e04d61712f3d4c6ddb6ade98474666
Jeff Casimir <jeff@casimircreative.com>
2013-02-07 07:58:21 -0700

    Make the display order actually autoincrement

--- app/parser/evaluator/parser/incrementing_display_order.rb
+++ app/parser/evaluator/parser/incrementing_display_order.rb
@@ -3,8 +3,12 @@ module Evaluator
     module IncrementingDisplayOrder
 
       def incrementing_display_order
-        @_display_order ||= 2
-        yield(@_display_order - 1) if block_given?
+        yield(next_display_order) if block_given?
+      end
+
+      def next_display_order
+        @_display_order ||= 0
+        @_display_order += 1
       end
 
     end
