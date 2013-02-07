0f92058e0105954e1155e694acd7cc725a4657aa
Jeff Casimir <jeff@casimircreative.com>
2013-02-07 11:09:31 -0700

    Fix ranges

--- app/evaluations/gschool/20130207_eventmangager_peer_assessment.rb
+++ app/evaluations/gschool/20130207_eventmangager_peer_assessment.rb
@@ -1,16 +1,16 @@
 project_evaluation "Peer Assessment for EventReporter" do
   section "Correctness" do
-    free_response "What score did they earn for correctness?", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "What score did they earn for correctness?", :kind => :integer, :group => :overall_score, :range => (0..4)
     free_response "If applicable, what problems did you run into?", :kind => :text
   end
 
   section "Style" do
-    free_response "What score did they earn for style?", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "What score did they earn for style?", :kind => :integer, :group => :overall_score, :range => (0..4)
     free_response "If applicable, what problems did you run into?", :kind => :text
   end
 
   section "Effort" do
-    free_response "What score did they earn for effort?", :kind => :integer, :group => :overall_score, :range => (0..3)
+    free_response "What score did they earn for effort?", :kind => :integer, :group => :overall_score, :range => (0..5)
     free_response "If applicable, what problems did you run into?", :kind => :text
   end
 
