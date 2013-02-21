62a0f61b750d2f79364a3d6711d39a6e50a3926a
Jeff Casimir <jeff@casimircreative.com>
2013-02-21 09:18:31 -0700

    Breaking up into three evals

---
+++ app/evaluations/gschool/20130221_sales_engine_peer_1.rb
@@ -0,0 +1,25 @@
+project_evaluation "Peer Assessment for SalesEngine - 1" do
+  section "Correctness" do
+    free_response "What score did they earn for correctness?", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "If applicable, what problems did you run into?", :kind => :text
+  end
+
+  section "Testing" do
+    free_response "What score did they earn for test coverage?", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "If applicable, what problems did you run into?", :kind => :text
+  end
+
+  section "Style" do
+    free_response "What score did they earn for style?", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "If applicable, what problems did you run into?", :kind => :text
+  end 
+
+  section "Effort" do
+    free_response "What score did they earn for effort?", :kind => :integer, :group => :overall_score, :range => (0..5)
+    free_response "If applicable, what problems did you run into?", :kind => :text
+  end
+
+  section "Overall" do
+    free_response "Any closing thoughts/recommendations?", :kind => :text
+  end
+end
