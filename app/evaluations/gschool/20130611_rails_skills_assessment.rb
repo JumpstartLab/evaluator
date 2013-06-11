4c98c0fb0e4a437be20d1c082b8613357459969a
Jeff Casimir <jeff@casimircreative.com>
2013-06-11 09:03:51 -0600

    Eval for skills assessment 3

---
+++ app/evaluations/gschool/20130611_rails_skills_assessment.rb
@@ -0,0 +1,12 @@
+instructor_evaluation "Skills Assessment 5/13" do
+  section "Skills" do
+    free_response "1. Ruby Syntax & API", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "2. Ruby Style", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "3. Rails Syntax & API", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "4. Rails Style", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "5. Testing", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "6. Workflow", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "7. Responding to Feedback / Collaboration", :kind => :integer, :group => :overall_score, :range => (0..4)
+    free_response "General comments:", :kind => :text
+  end
+end
